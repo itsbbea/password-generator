30e30e33328d63d5bc92cd86565299a275620ddd
itsbbea <bea.may27@hotmail.com>
2023-09-07 18:14:56 +1000

    updated script.js file with prompt and confirm method in order for user to type their preferred length and number of characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var length = 8;
+var length = 8 > 128;
 
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
@@ -10,25 +10,40 @@ var char = "!@#$%^&*()";
 var allChars = upperCase + lowerCase + number + Symbol;
 
 // Write password to the #password input
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  var password = "";
-  password += upperCase [Math.floor(Math.random() * upperCase.length)];
-  password += lowerCase [Math.floor(Math.random() * lowerCase.length)];
-  password += number [Math.floor(Math.random() * number.length)];
-  password += char [Math.floor(Math.random() * char.length)];
+}
 
-  while(length > password.length) {
-    password += allChars[Math.floor(Math.random() * allChars.length)];
+var password = "";
+password += upperCase[Math.floor(Math.random() * upperCase.length)];
+password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+password += number[Math.floor(Math.random() * number.length)];
+password += char[Math.floor(Math.random() * char.length)];
 
-  }
-
-  passwordText.value = password;
+while (length > password.length) {
+  password += allChars[Math.floor(Math.random() * allChars.length)];
 
 }
 
-// //Add event listener to generate butto
-generateBtn.addEventListener("click" , writePassword);
+var passwordLength = prompt("Please enter the number of characters");
+var numbers = confirm("Do you want numbers in your password?");
+var lowerCases = confirm("Do you want lower cases in your password?");
+var upperCases = confirm("Do you want upper cases in your password?");
+var special = confirm("Do you want special characters in your password?");
+
+var minimumCount = 0;
+
+var minimumNumbers = "";
+var minimumLowerCases = "";
+var minimumUpperCases = "";
+var minimumSpecialCharacters = "";
+
+passwordText.value = password;
+
+// //Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
