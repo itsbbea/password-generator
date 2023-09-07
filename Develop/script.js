0755623a7cbfef087905b1f07b4f1bd337edec54
itsbbea <bea.may27@hotmail.com>
2023-09-07 18:36:36 +1000

    updated script.js file with prompt and confirm method for user to inpur their desired password length and number of characters

--- Develop/script.js
+++ Develop/script.js
@@ -13,36 +13,44 @@ var allChars = upperCase + lowerCase + number + Symbol;
 
 
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  var password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += char[Math.floor(Math.random() * char.length)];
+
+  if(passwordLength < 8) {
+    alert("Length cannot be less than 8!")
+} else if (passwordLength > 128) {
+    alert("Length cannot be more than 128")
 }
 
-var password = "";
-password += upperCase[Math.floor(Math.random() * upperCase.length)];
-password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-password += number[Math.floor(Math.random() * number.length)];
-password += char[Math.floor(Math.random() * char.length)];
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
 
-while (length > password.length) {
-  password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+
+  var passwordLength = prompt("Please enter the number of characters");
+  var numbers = confirm("Do you want numbers in your password?");
+  var lowerCases = confirm("Do you want lower cases in your password?");
+  var upperCases = confirm("Do you want upper cases in your password?");
+  var special = confirm("Do you want special characters in your password?");
+
+  var minimumCount = 0;
+
+  var minimumNumbers = "";
+  var minimumLowerCases = "";
+  var minimumUpperCases = "";
+  var minimumSpecialCharacters = "";
+
+  passwordText.value = password;
 
 }
 
-var passwordLength = prompt("Please enter the number of characters");
-var numbers = confirm("Do you want numbers in your password?");
-var lowerCases = confirm("Do you want lower cases in your password?");
-var upperCases = confirm("Do you want upper cases in your password?");
-var special = confirm("Do you want special characters in your password?");
 
-var minimumCount = 0;
-
-var minimumNumbers = "";
-var minimumLowerCases = "";
-var minimumUpperCases = "";
-var minimumSpecialCharacters = "";
-
-passwordText.value = password;
 
 // //Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
